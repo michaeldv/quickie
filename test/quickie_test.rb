005e8721666947c0e459ff286fe8a5dfa0da87d0
Mike Dvorkin <mike@dvorkin.net>
2012-01-06 19:00:44 -0800

    Implemented method stubs

---
+++ test/quickie_test.rb
@@ -0,0 +1,7 @@
+# Copyright (c) 2011-12 Michael Dvorkin
+#
+# Quickie is freely distributable under the terms of MIT license.
+# See LICENSE file or http://www.opensource.org/licenses/mit-license.php
+#------------------------------------------------------------------------------
+require File.expand_path(File.dirname(__FILE__) + "/matcher_test")
+require File.expand_path(File.dirname(__FILE__) + "/stub_test")
